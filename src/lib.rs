b346bca5eb0b69c86a03cc4df935d78223ebe6dc
Marlon de Oliveira dos Santos <marlon@multipedidos.com.br>
2024-03-21 19:55:43 -0300

    pacote de impressão

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,15 @@
 #![deny(clippy::all)]
 
 use napi_derive::napi;
-use printers;
+use marlon_printers;
 
 #[napi]
 pub fn get_printers() -> Vec<String> {
-  return printers::get_printers().into_iter().map(|printer| printer.name).collect();
+  return marlon_printers::get_printers().into_iter().map(|printer| printer.name).collect();
 }
 
 #[napi]
 pub fn print(printer_name: String, buffer: &[u8]) {
-  let printer = printers::get_printer_by_name(&printer_name).unwrap();
+  let printer = marlon_printers::get_printer_by_name(&printer_name).unwrap();
   let _ = printer.print(buffer, None);
 }
